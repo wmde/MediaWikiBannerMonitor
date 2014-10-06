c71cd1d5fac86e4875afebafb03ac87be9f5581a
WMDE-Fisch <christoph.fischer@wikimedia.de>
2014-10-06 14:49:37 +0200

    added conf files

---
+++ conf/local-config.sample.inc.php
@@ -0,0 +1,4 @@
+<?php
+define( 'META_API_URL', 'https://meta.wikimedia.org/w/api.php' );
+define( 'MAIL_SENDER_EMAIL', 'bannermonitor@wikimedia.de' );
+define( 'MAIL_SENDER_NAME', 'Banner Monitor Script' );
